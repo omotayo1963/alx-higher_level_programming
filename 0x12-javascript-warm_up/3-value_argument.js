12992caf69f12a2f06c12afdf8a45d70f5150a3c
omotayo1963 <adeniyimuizudeen40@gmail.com>
2023-03-15 22:36:23 +0000

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const { argv } = require('process');
-if (argv[2] === undefined) { console.log('No argument'); } else { console.log(argv[2]); }
-
+if (!process.argv[2]) console.log('No argument');
+else console.log(process.argv[2]);
